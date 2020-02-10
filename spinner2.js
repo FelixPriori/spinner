abf606b97d2b9e39ec32da92bc0d4b6b0994f79c
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-10 19:50:32 +0000

    refactor of spinner in less lines

--- spinner2.js
+++ spinner2.js
@@ -1,17 +1,12 @@
 const spintit = () => {
   process.stdout.write('hello from spinner1.js... \rheyyy\n');
-
   timer = 100;
   animation = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   '];
-
   for (let i = 0; i < animation.length; i++) {
-
     setTimeout(() => {
       process.stdout.write(animation[i]);
     }, timer);
-
     timer += 200;
-
   }
 }
 
